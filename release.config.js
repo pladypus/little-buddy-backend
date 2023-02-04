3ceca933fc191e0824f31241ab05963f0fc8d2a7
Peter Loftus <loftusp5976@gmail.com>
2023-02-04 18:14:40 -0500

    ci: added semantic-release config file

---
+++ release.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
